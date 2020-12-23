d154d71885db470badf7f8b3378c6efd4112bd2f
Peter Marshall <peter.marshall@blindsidenetworks.com>
2020-12-23 11:16:56 -0500

    Create a seed to populate the database

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy]
-      resources :boards, only: [:show, :create, :update, :destroy, :index]
+      resources :boards, only: [:show, :create, :update, :destroy, :index] do
+        resources :cards
+      end
       resources :tokens, only: [:create]
     end
   end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.delete_all
+User.delete_all
+
+5.times do
+    user = User.create! email: Faker::Internet.email, password: 'password1234'
+    puts "Created a new user: #{user.email}"
+
+    3.times do
+        board = Board.create!(
+            name: Faker::App.name,
+            user: user
+        )
+        puts "Created a new board for #{user.email}: #{board.name}"
+    end
+end
