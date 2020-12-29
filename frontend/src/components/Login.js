5da9c0ea919102cd3beb753606d7cddc03386ceb
Peter Marshall <peterdmarshall99@gmail.com>
2020-12-28 21:15:32 -0500

    Initial styling for login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,14 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { FormControlLabel, Checkbox, Button, Paper, TextField, CircularProgress, Container, Grid, Box, Typography } from '@material-ui/core';
 
 import { userActions } from '../actions/user.actions';
+
+const useStyles= makeStyles({
+    root: {
+        background: 'linear-gradient(70deg, #3642CF, #BF37AD)',
+        minHeight: '100vh'
+    },
+    paper: {
+        width: '50vw',
+        height: '50vh',
+        marginTop: '10vw',
+        paddingTop: '5vh',
+        paddingBottom: '5vh'
+    },
+    inputField: {
+        width: '35vw',
+        marginBottom: '1vh'
+    },
+    loginButton: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: '6vh',
+        width: '35vw',
+        padding: '0 30px',
+    },
+    signupLink: {
+        marginTop: '2vh'
+    }
+});
 
 function Login(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [submitted, setSubmitted] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [rememberMe, setRememberMe] = useState(false);
     const history = useHistory();
+    const classes = useStyles();
 
     const { dispatch, loggingIn, loggedIn } = props;
 
@@ -28,7 +65,18 @@ function Login(props) {
         setSubmitted(true);
         if(email && password) {
             dispatch(userActions.login(email, password));
+        } else if (password) {
+            setEmailError(true);
+        } else if (email) {
+            setPasswordError(true);
+        } else {
+            setEmailError(true);
+            setPasswordError(true);
         }
+    }
+
+    const loginDemoUser = () => {
+        // Login with a demo user that has populated example data
     }
 
     if(loggedIn) {
@@ -36,32 +84,71 @@ function Login(props) {
     }
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form name="form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="text" name="email" value={email} onChange={handleEmailChange} />
-                    { submitted && !email &&
-                        <div>Email is required</div>
-                    }
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-                    { submitted && !password &&
-                        <div>Password is required</div>
-                    }
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                    { loggingIn &&
-                        <p>Logging In ...</p>
-                    }
-                </div>
-            </form>
-        </div>
-
+        <Box className={classes.root}>
+        <Container>
+            <Grid container justify="center">
+            <Paper elevation={0} className={classes.paper}>
+            <Grid container justify="center">
+                <Grid container item xs={9} spacing={3} direction="column">
+                    <Grid item container justify="center">
+                        <h1>Login</h1>
+                    </Grid>
+                    <Grid item>
+                    <form name="form" onSubmit={handleSubmit}>
+                        <div>
+                            <TextField
+                                id="email-field"
+                                label={emailError ? "Error" : "Email"}
+                                value={email}
+                                onChange={handleEmailChange}
+                                variant="filled"
+                                className={classes.inputField}
+                            />
+                        </div>
+                        <div>
+                            <TextField
+                                id="password-field"
+                                type="password"
+                                label={passwordError ? "Error" : "Password"}
+                                value={password}
+                                onChange={handlePasswordChange}
+                                variant="filled"
+                                className={classes.inputField}
+                            />
+                        </div>
+                        <div>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={rememberMe}
+                                        onChange={() => setRememberMe(!rememberMe)}
+                                        name="rememberMe"
+                                        color="primary"
+                                    />
+                                }
+                                label="Remember Me"
+                            />
+                        </div>
+                        <div>
+                            {!loggingIn && <Button className={classes.loginButton} type="submit">Login</Button>}
+                            { loggingIn && <CircularProgress />}
+                        </div>
+                    </form>
+                    <Typography className={classes.signupLink}>
+                        Don't have an account?
+                        <Link to="/signup">Sign Up here.</Link>
+                    </Typography>
+                    <Typography className={classes.signupLink}>
+                        Just visiting?
+                        <Link to="/" onClick={() => loginDemoUser()}>Log in as a demo user.</Link>
+                    </Typography>
+                    </Grid>
+                </Grid>
+            </Grid>
+            </Paper>
+            </Grid>
+        </Container>
+        </Box>
     );
 }
 
