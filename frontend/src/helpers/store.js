7ba8ea14a9b919aabd613e3fc0966f552d81aad3
Peter Marshall <peter.marshall@blindsidenetworks.com>
2020-12-29 17:47:12 -0500

    Add signup functionality. Create user reducer to handle actions related to user accounts.

--- frontend/src/helpers/store.js
+++ frontend/src/helpers/store.js
@@ -4,10 +4,11 @@ import { createLogger } from 'redux-logger';
 import { alert } from '../reducers/alert.reducer';
 import { authentication } from '../reducers/authentication.reducer';
 import { board } from '../reducers/board.reducer';
+import { user } from '../reducers/user.reducer';
 
 const loggerMiddleware = createLogger();
 
-const rootReducer = combineReducers({ alert: alert, authentication: authentication, board: board });
+const rootReducer = combineReducers({ alert: alert, authentication: authentication, board: board, user: user });
 
 export const store = createStore(
     rootReducer,
